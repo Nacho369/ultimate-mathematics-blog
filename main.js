e39c2ac55423092ca35b7bc768d5e957151e1259
Fortune Iheanacho <nachodev369@gmail.com>
2022-09-27 23:30:56 +0100

    Added List of Registered Participant

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 const header = document.querySelector('header');
-const navLinks = document.querySelectorAll('.navLinks li');
+const logo = document.querySelector('.logoFlex img');
+const navLinks = document.querySelectorAll('.navLinks a');
 
 window.addEventListener('scroll', () => {
     if(this.scrollY > 100){
@@ -8,33 +9,17 @@ window.addEventListener('scroll', () => {
         navLinks.forEach(navLink => {
             navLink.classList.add("navChg");
         });
-        
+
+        logo.src = './images/umc_logo_full_white.png';
     }
     else{
         header.classList.remove("headerBg");
         navLinks.forEach(navLink => {
             navLink.classList.remove("navChg");
         });
+
+        logo.src = './images/umc_logo_full.png';
     }
 })
 
 
-let noOfStudents = document.querySelector('.noOfStudents');
-let noOfExams = document.querySelector('.noOfExams');
-let addNoOfStud = parseInt(noOfStudents.innerHTML);
-let addNoOfExam = parseInt(noOfExams.innerHTML);
-window.addEventListener('load', () => {
-    
-    // noOfStudents.innerHTML = 2;
-    let counter = 20;
-    const countUp = () => {
-        if(addNoOfStud < counter && addNoOfExam < counter){
-            addNoOfStud++;
-            addNoOfExam++;
-            noOfStudents.innerHTML = addNoOfStud;
-            noOfExams.innerHTML = addNoOfExam;
-        }
-    }
-    setInterval(countUp, 1000);
-    
-})
